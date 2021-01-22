a25b99a6a1b8d4fd6edb68271bc190a87f3f4027
MainulTamim <tamim8332@gmail.com>
2021-01-22 17:16:17 +0600

    all

--- assignment.js
+++ assignment.js
@@ -1,45 +1,53 @@
-const kilometerToMeter = (kilometer) =>{
-    let meter = kilometer*1000;
+// https://github.com/MainulTamim/assingment.js
+
+const kilometerToMeter = (kilometer) => {
+    let meter = kilometer * 1000;
     return meter;
 };
-const budgetCalculator = (watch, mobile, laptop) =>{
-    let totalPrice = 50*watch+100*mobile+500*laptop;
+
+
+const budgetCalculator = (watch, mobile, laptop) => {
+    let totalPrice = 50 * watch + 100 * mobile + 500 * laptop;
     return totalPrice;
 };
-const hotelCost = (days) =>{
-    first=0;
-    second=0;
-    third=0;
-                    if(days>10){
-        first = 10*100;
-        days = days-10;
-        if(days>10){
-            second = 10* 80;
-            days = days -10;
+
+
+const hotelCost = (days) => {
+    let first = 0;
+    let second = 0;
+    let third = 0;
+    if (days > 10) {
+        first = 10 * 100;
+        days = days - 10;
+        if (days > 10) {
+            second = 10 * 80;
+            days = days - 10;
         }
-        else{
-            second = days*80;
-            days=0;
+        else {
+            second = days * 80;
+            days = 0;
         }
-        if(days>0){
-            third= days*50;
+        if (days > 0) {
+            third = days * 50;
         };
     }
-    else{
-        first = days*100; 
+    else {
+        first = days * 100;
     }
     total = first + second + third;
     return total;
 };
 
-const findLongestWord =(friends) =>{
-    var longestName = "";
-  
-    friends.forEach(function(name) {
-      if(name.length > longestName.length) {
-        longestName = name;
-      }
+
+
+const megaFriend = (friends) => {
+    let longestName = "";
+
+    friends.forEach(function (name) {
+        if (name.length > longestName.length) {
+            longestName = name;
+        }
     });
-  
+
     return longestName;
-  }
+};
